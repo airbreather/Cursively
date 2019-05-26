b2acfaa975c2730d4ace57be04a325bf7f6e8df5
Joe Amenta <airbreather@linux.com>
2019-05-26 09:45:37 -0400

    Add stuff to generate docs.

--- calculate-coverage.cmd
+++ calculate-coverage.cmd
@@ -1,24 +1,24 @@
 pushd %~dp0
-nuget install OpenCover -Version 4.7.922 -OutputDirectory OpenCover\tools
-nuget install ReportGenerator -Version 4.1.4 -OutputDirectory OpenCover\tools
+nuget install OpenCover -Version 4.7.922 -OutputDirectory tools
+nuget install ReportGenerator -Version 4.1.4 -OutputDirectory tools
 dotnet build -c Release
 for /F "tokens=* USEBACKQ" %%D in (`where dotnet`) do (
 set DotNetPath=%%D
 )
-OpenCover\tools\OpenCover.4.7.922\tools\OpenCover.Console.exe ^
+tools\OpenCover.4.7.922\tools\OpenCover.Console.exe ^
     "-target:%DotNetPath%" ^
     "-targetArgs:test -c Release --no-build" ^
     "-filter:+[Cursively]* +[Cursively.*]* -[Cursively.Tests]*" ^
-    -output:OpenCover\raw-coverage-results.xml ^
+    -output:tools\raw-coverage-results.xml ^
     -register:user ^
     -oldstyle
 
 dotnet clean -c Release
 
-dotnet OpenCover\tools\ReportGenerator.4.1.4\tools\netcoreapp2.1\ReportGenerator.dll ^
-    -reports:OpenCover\raw-coverage-results.xml ^
-    -targetdir:OpenCover\results
+dotnet tools\ReportGenerator.4.1.4\tools\netcoreapp2.1\ReportGenerator.dll ^
+    -reports:tools\raw-coverage-results.xml ^
+    -targetdir:tools\results
 
-OpenCover\results\index.htm
+tools\results\index.htm
 
 popd
