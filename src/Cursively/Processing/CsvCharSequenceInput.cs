a777b7e80b1fd77c4f35015c4e305ffd3c1a164c
Joe Amenta <airbreather@linux.com>
2019-06-16 11:54:32 -0400

    pin it the whole time

--- src/Cursively/Processing/CsvCharSequenceInput.cs
+++ src/Cursively/Processing/CsvCharSequenceInput.cs
@@ -62,35 +62,37 @@ namespace Cursively.Processing
                 bytes = new byte[maxByteCount];
             }
 
-            foreach (var segment in _chars)
+            fixed (byte* b = &bytes[0])
             {
-                var charSpan = segment.Span;
-                if (charSpan.IsEmpty)
+                foreach (var segment in _chars)
                 {
-                    continue;
-                }
-
-                int rem = charSpan.Length;
-                fixed (byte* b = &bytes[0])
-                fixed (char* cHead = &charSpan[0])
-                {
-                    char* c = cHead;
-
-                    while (rem > chunkCharCount)
+                    var charSpan = segment.Span;
+                    if (charSpan.IsEmpty)
                     {
-                        int byteCount = encoding.GetByteCount(c, chunkCharCount);
-                        encoding.GetBytes(c, chunkCharCount, b, byteCount);
-                        tokenizer.ProcessNextChunk(new ReadOnlySpan<byte>(b, byteCount), visitor);
-
-                        rem -= chunkCharCount;
-                        c += chunkCharCount;
+                        continue;
                     }
 
-                    if (rem > 0)
+                    int rem = charSpan.Length;
+                    fixed (char* cHead = &charSpan[0])
                     {
-                        int byteCount = encoding.GetByteCount(c, rem);
-                        encoding.GetBytes(c, rem, b, byteCount);
-                        tokenizer.ProcessNextChunk(new ReadOnlySpan<byte>(b, byteCount), visitor);
+                        char* c = cHead;
+
+                        while (rem > chunkCharCount)
+                        {
+                            int byteCount = encoding.GetByteCount(c, chunkCharCount);
+                            encoding.GetBytes(c, chunkCharCount, b, byteCount);
+                            tokenizer.ProcessNextChunk(new ReadOnlySpan<byte>(b, byteCount), visitor);
+
+                            rem -= chunkCharCount;
+                            c += chunkCharCount;
+                        }
+
+                        if (rem > 0)
+                        {
+                            int byteCount = encoding.GetByteCount(c, rem);
+                            encoding.GetBytes(c, rem, b, byteCount);
+                            tokenizer.ProcessNextChunk(new ReadOnlySpan<byte>(b, byteCount), visitor);
+                        }
                     }
                 }
             }
